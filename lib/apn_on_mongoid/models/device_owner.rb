e0f996a309ec42fb588c68027bd84bb39ffbc604
Olivier Berho <olivier.berho@gmail.com>
2012-01-11 15:33:09 +0100

    managing feedbacks from apple

--- lib/apn_on_mongoid/models/device_owner.rb
+++ lib/apn_on_mongoid/models/device_owner.rb
@@ -3,7 +3,7 @@ module APN
     extend ActiveSupport::Concern
     
     included do
-      embeds_many :devices, :class_name => 'APN::Device', dependent: :delete
+      embeds_many :devices, :class_name => 'APN::Device'
     end
     
     module InstanceMethods
